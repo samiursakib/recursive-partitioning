ca89d2ea638aa2091583e2b375ecd9aece8d4850
samiursakib <rafisamiur@gmail.com>
2024-05-22 01:37:33 +0600

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const child = {
+    name: 'sakib',
+    child: null,
+  };
+  const data = {
+    name: 'salahuddin',
+    child: null,
+  };
+  const onClick = () => {
+    data.child = child;
+    console.log(data);
+  };
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={onClick}>
+      Hello
     </div>
   );
 }
